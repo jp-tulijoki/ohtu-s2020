00f8540c6b25535a2d673fdf67d3fff95d24c977
Juha-Pekka Tulijoki <jp.tulijoki@gmail.com>
2020-11-09 18:48:27 +0200

    tehtävät 9-12

--- viikko2/tehtavat1ja9-12/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kirjanpito.java
+++ viikko2/tehtavat1ja9-12/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kirjanpito.java
@@ -2,21 +2,16 @@
 package ohtu.verkkokauppa;
 
 import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Kirjanpito {
-    private static Kirjanpito instance;
-    
-    public static Kirjanpito getInstance() {
-        if ( instance==null) {
-            instance = new Kirjanpito();
-        }
-        
-        return instance;
-    }
     
     private ArrayList<String> tapahtumat;
 
-    private Kirjanpito() {
+    @Autowired
+    public Kirjanpito() {
         tapahtumat = new ArrayList<String>();
     }
     
